1e6aa92dfd832d0d8e1c5101332eb7d6d95054dd
iNoeigHt <franciscoignaciolq@gmail.com>
2024-07-15 23:23:19 -0400

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './app.css';
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Post It Simulator!</h1>
+            <div className="form">
+                <input className="inputs" type="text" id="titulo" placeHolder="Titulo" />
+                <input className="inputs" type="text" id="descripcion" placeHolder="Descripcion" />
+                <label>
+                    <input className="inputs" type="checkbox" id="importante" /> Importante!
+                </label>
+                <button id="boton_agregar">AGREGAR</button>
+            </div>
+            <div className="block_notas">
+                    
+            </div>
+        </div>
+    )
+}
+
+export default App;
