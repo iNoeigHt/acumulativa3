d856536f8bc2d06950880a344aa4571593073b9b
iNoeigHt <franciscoignaciolq@gmail.com>
2024-07-19 12:53:55 -0400

    commit 5

--- src/componentes/Form.jsx
+++ src/componentes/Form.jsx
@@ -6,51 +6,54 @@ import { v4 as uuid } from 'uuid';
 
 export function FormPost() {
 
-    const [postList, setPostList] = useState([
-        {id:uuid(), titulo: 'Tarea 1', descripcion: 'descripcion 1', importante: true},
-        {id:uuid(), titulo: 'Tarea 2', descripcion: 'descripcion 2', importante: false},
-        {id:uuid(), titulo: 'Tarea 3', descripcion: 'descripcion 3', importante: true},
-        {id:uuid(), titulo: 'Tarea 4', descripcion: 'descripcion 4', importante: false},
-        {id:uuid(), titulo: 'Tarea 5', descripcion: 'descripcion 5', importante: true}]);
-
     const tituloRef = useRef();
     const descripcionRef = useRef();
     const importanteRef = useRef();
+    const [mensaje, setMensaje] = useState('');
 
-    function addPost() {
+    const [postList, setPostList] = useState([
+       ]);
+
+    function addPost(e) {
 
         const titulo = tituloRef.current.value;
         const descripcion = descripcionRef.current.value;
         const importante = importanteRef.current.checked;
 
-        if(importante.trim() === ''){
-            console.log('campos vacios');
+        if(descripcion.trim() === ''){
+            setMensaje('Campos vacios');
+
+            setTimeout(() => {
+                setMensaje('');
+            }, 3000);
+
             return
-            }
-            
+        }
 
-        setPostList(() => {
+        const newPost = {
+            id: uuid(), 
+            titulo: titulo,
+            descripcion: descripcion,
+            importante: importante
+        }
 
-            const newPost = {
-                id:uuid(), 
-                titulo:titulo,
-                descripcion:descripcion,
-                importante:importante
-            }
+        e.preventDefault();
+        const newPostList = [...postList, newPost];
+        setPostList(newPostList);
 
-            const newPostList = [...postList, newPost];
-            return newPostList;
-        });
+        alert("se agrego el Post");
     }
 
     const eliminarPost = id => {
         const newPostList = postList.filter(nota => nota.id !== id);
         setPostList(newPostList);
+
+        alert("se elimino el Post")
     }
 
     return (
         <div className="container">
-            <h1>Post It Simulator!</h1>
+            <h1 className='tituloPrincipal'>Post It Simulator!</h1>
             <div>
                 <form>
                     <input ref={tituloRef} className="inputs" placeholder="Titulo" type="text" id="titulo"/>
@@ -63,7 +66,7 @@ export function FormPost() {
             </div>
             <div className="block_notas">
                 {
-                    postList.map(nota => <Post titulo={nota.titulo} descripcion={nota.descripcion} importante={nota.importante} key={nota.id} eliminarPost={eliminarPost}/>)
+                    postList.map(nota => <Post id={nota.id} titulo={nota.titulo} descripcion={nota.descripcion} importante={nota.importante} key={nota.id} eliminarPost={eliminarPost}/>)
                 }
             </div>
         </div>
--- src/componentes/Postit.jsx
+++ src/componentes/Postit.jsx
@@ -4,33 +4,15 @@ import { AiOutlineClose } from "react-icons/ai";
 
 export function Post({id, titulo, descripcion, importante, eliminarPost}) {
 
-    const PostId = id;
     const PostTitulo = titulo;
     const PostDescrip = descripcion;
-    const PostImp = importante;
 
     if(importante) {
         return (
-            <div className="contenedor">
-                <div className="post">
-                    <h1 className="tituloPost"></h1> 
-                    <p className="descripcionPost"></p>
-                    <div 
-                        className="icono-eliminar"
-                        onClick={() => eliminarPost(id)}>
-                        <AiOutlineClose />
-                    </div>
-                </div> 
-            </div>
-        )
-    }
-
-    else {
-        return (
             <div className="contenedorImportante">
                 <div className="post">
-                    <h1 className="tituloPost"></h1> 
-                    <p className="descripcionPost"></p>
+                    <h1 className="tituloPost">{PostTitulo}</h1> 
+                    <p className="descripcionPost">{PostDescrip}</p>
                     <div 
                         className="icono-eliminar"
                         onClick={() => eliminarPost(id)}>
@@ -39,5 +21,20 @@ export function Post({id, titulo, descripcion, importante, eliminarPost}) {
                 </div> 
             </div>
     )
+    }
+    else {
+        return (
+            <div className="contenedor">
+                <div className="post">
+                    <h1 className="tituloPost">{PostTitulo}</h1> 
+                    <p className="descripcionPost">{PostDescrip}</p>
+                    <div 
+                        className="icono-eliminar"
+                        onClick={() => eliminarPost(id)}>
+                        <AiOutlineClose />
+                    </div>
+                </div> 
+            </div>
+        )
     }
 }
